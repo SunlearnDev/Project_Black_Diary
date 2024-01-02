0909356820291e4cd08b5639b22ca45b7fb9f716
Nguyen Lam <lamn.1440101226785@vtc.edu.vn>
2024-01-02 13:42:14 +0700

    142

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,10 +15,10 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-// window._ = require('lodash');
-// window.$ = window.jQuery = require('jquery');
-// require('bootstrap-sass');
-// var notifications = [];
-// const NOTIFICATION_TYPES = {
-//    follow: 'App\\Notifications\\FollowNotification',
-// };
+window._ = require('lodash');
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
+var notifications = [];
+const NOTIFICATION_TYPES = {
+   follow: 'App\\Notifications\\FollowNotification',
+};
