e84d736ee803b249c51756968541887d7bc40934
NL99 <122776493+NgL1999@users.noreply.github.com>
2024-01-05 11:41:37 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    @include('Fontend.layouts.style')
     @stack('style')
 </head>
 
